6b8c6324fe8b63f231df1f8c07f0c13674d13ed3
Prqm <Prqm@qq.com>
2022-07-18 16:48:06 +0800

    second commit

---
+++ src/com/atguigu/Test.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author 旁人清明
+ * @CREATE 2022-07-18 16:42
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("1111111");
+        System.out.println("2222222");
+    }
+}
